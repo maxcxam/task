d60281001bf37faa16421f8b76e9f23163de9d5a
Maxcxam <maxcxam1979@gmail.com>
2018-11-19 17:57:47 +0200

    first commit

---
+++ custom.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+	$('#carousel1').owlCarousel({
+	    loop:true,
+	    nav:false,
+	    dots:true	    
+	});
+
+	$('#carousel2').owlCarousel({
+	    loop:false,
+	    nav:true,
+	    dots:false	    
+	});
+		
+})
